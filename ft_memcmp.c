52b4ffaf7084adf6947063a6d4be18c1e5f41049
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-10-24 21:28:21 +0200

    Create ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,21 @@
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t				i;
+	unsigned const char	*str1;
+	unsigned const char	*str2;
+
+	i = 0;
+	str1 = ((unsigned char *)s1);
+	str2 = ((unsigned char *)s2);
+	while (i < n)
+	{
+		if (*str1 != *str2)
+		{
+			return ((int)(*str1 - *str2));
+		}
+		str1++;
+		str2++;
+		i++;
+	}
+	return (0);
+}
