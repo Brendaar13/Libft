df39dc44afe3378b9aaa5409fb870b1329832b39
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-10-24 21:52:11 +0200

    Create ft_lstclear_bonus.c

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,14 @@
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*aux;
+
+	if (!*lst)
+		return ;
+	while (*lst)
+	{
+		aux = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = aux;
+	}
+	*lst = 0;
+}
