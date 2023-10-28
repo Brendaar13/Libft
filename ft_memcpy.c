a54b5abbf4bffca90acae71a10a0bdfd9ef04f5e
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-10-28 23:25:41 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,15 +1,16 @@
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
+	unsigned char *aux_dst;
+	unsigned char *aux_src;	
+	
 	if (src == NULL && dst == NULL)
 		return (NULL);
-
-	char *aux_dst = (char *)dst;
-	char *aux_src = (char *)src;
-
+	aux_dst = (unsigned char *)dst;
+	aux_src = (unsigned char *)src;
 	while (n--)
 	{
 		*aux_dst++ = *aux_src++;
-	};
+	}
 	return (dst);
 }
 
