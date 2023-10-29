c5ace30af604550bbd21d6bdbe31039d7867e446
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-10-29 15:38:05 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -22,3 +22,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
+
+int main ()
+{
+	char *haystack = "Testeo de esta shit";
+	char *needle = "shit";
+	char *result = ft_strnstr(haystack, needle, 50);
+
+	printf("%s\n", result);
+	return (0);
+}
