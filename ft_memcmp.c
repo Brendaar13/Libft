0c2fee788ee966138abdf92988850cb7a3399162
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-10-29 15:41:18 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -19,3 +19,16 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
+
+int main()
+{
+	char *s1 = {0x13};
+	char *s2 = {0x13, 0x77, 0x88, 0x93};
+	size_t n = sizeof(s1);
+
+	int result = ft_memcmp(s1, s2, n);
+
+	printf("0x%02X\n", result);
+	return (0)
+		;
+}
