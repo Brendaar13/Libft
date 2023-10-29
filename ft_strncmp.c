0e443a46013f897c94272153187a56d844c6a139
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-10-29 15:42:19 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -13,3 +13,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
+
+int main()
+{
+	char *s1 = "Tengo mucho sueño y hambre";
+	char *s2 = "Tengo mucha hambre";
+	size_t n = 7;
+
+	int result = ft_strncmp(s1, s2, n);
+
+	printf("%d\n", result);
+	return (0);
+}
