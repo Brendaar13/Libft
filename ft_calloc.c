e674a8db6ffc6bf1a51499b87a9257e667132a35
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-04 20:51:52 +0100

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -8,3 +8,19 @@ void	*ft_calloc(size_t count, size_t size)
 	ft_bzero(ptr, size * count);
 	return (ptr);
 }
+int main ()
+{
+	size_t	count = 5;
+	size_t	size = sizeof(size_t);
+	size_t	*array = ft_calloc(count, size);
+
+	if (array)
+	{
+		printf("%zu\n", array[0]);
+		if (count > 1)
+			printf("%zu\n", array[1]);
+		if (count > 2)
+			printf("%zu\n", array [2]);
+	}
+		return (0);
+}
