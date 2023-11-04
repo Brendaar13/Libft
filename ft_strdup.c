912d18c2ab6c16fb0578d71e98ecec96931f8919
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-04 21:42:55 +0100

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -11,3 +11,14 @@ char	*ft_strdup(const char *s1)
 	ptr[size] = '\0';
 	return (ptr);
 }
+
+int main ()
+{
+	char *org = "Ver el mundo arder";
+	char *dup = ft_strdup(org);
+
+	printf("Original: %s\n", org);
+	printf("Duplicado: %s\n", dup);
+
+	return (0);
+}
