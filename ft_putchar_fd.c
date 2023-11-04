531380c428d2a2d8a5ccfed6f8161a670c8c483c
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-05 00:56:06 +0100

    Update ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -3,3 +3,15 @@ void	ft_putchar_fd(char c, int fd)
 	if (fd >= 0)
 		write(fd, &c, 1);
 }
+
+int main ()
+{
+	int fd = 1;
+	char c = 'A';
+	char *result = ft_putchar_fd(c, fd);
+
+	printf("%c\n", result);
+	return (0);
+}
+
+aaaaaaa mis neuronas
