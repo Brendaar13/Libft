4f2812c5620bcda2d623760d3e9ced0b7bb2c8dc
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-05 10:05:18 +0100

    Update ft_lstsize_bonus.c

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -10,3 +10,17 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (count);
 }
+
+int main ()
+{
+    t_list *node1 = (t_list *)malloc(sizeof(*node1));
+    t_list *node2 = (t_list *)malloc(sizeof(*node2));
+
+	node1->next = node2;
+
+    int size = ft_lstsize(node1);
+
+    printf("Tamaño de la lista: %d\n", size);
+
+	return (0);
+}
