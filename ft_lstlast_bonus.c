6063d703a1874bfc4d9d8bc743536d01712ff3de
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-05 10:11:51 +0100

    Update ft_lstlast_bonus.c

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -8,3 +8,19 @@ t_list	*ft_lstlast(t_list *lst)
 	}
 	return (lst);
 }
+
+int main ()
+{
+    t_list *node1 = (t_list *)malloc(sizeof(*node1));
+    t_list *node2 = (t_list *)malloc(sizeof(*node2));
+
+	node1->next = node2;
+
+    t_list *last = ft_lstlast(node1);
+
+    printf("Contenido último nodo: %p\n", last-> content);
+
+	return (0);
+}
+
+creo que esta bien(?
