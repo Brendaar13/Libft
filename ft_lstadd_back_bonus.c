05c1cba928f4d846e4d7ee27ab02bc235b9d764f
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-05 09:46:04 +0100

    Update ft_lstadd_back_bonus.c

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -12,3 +12,28 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	aux = ft_lstlast(*lst);
 	aux->next = new;
 }
+
+int main() {
+    t_list *list = NULL;
+    int data1 = 55;
+	int data2 = 42;
+
+	t_list *node1 = (t_list *)malloc(sizeof(*node1));
+    node1->content = &data1;
+    node1->next = NULL;
+
+    ft_lstadd_back(&list, node1);
+
+	t_list *node2 = (t_list *)malloc(sizeof(*node2));
+    node2->content = &data2;
+    node2->next = NULL;
+
+	ft_lstadd_back(&list, node2);
+
+    t_list *current = list;
+
+	printf("Data: %d\n", *((int *)(current->content)));
+    current = current->next;
+
+    return 0;
+}
