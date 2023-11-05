0cff83e83f238b42a259c9b6c36b4f93a3ffa181
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-05 01:18:32 +0100

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -11,3 +11,16 @@ void	ft_putstr_fd(char *s, int fd)
 		i++;
 	}
 }
+
+int main ()
+{
+	int fd = 2;
+	char *str = "Quiero un coco";
+
+	printf("%d\n", fd);
+	ft_putstr_fd(str, fd);
+
+	return (0);
+}
+
+npi si funciona
