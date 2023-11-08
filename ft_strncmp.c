41c5065a317852dde146765ee25946887a9b3be3
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-08 16:34:01 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,19 +1,17 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n && (s1[i] || s2[i]))
+	while (n > 0)
 	{
-		if (s1[i] != s2[i])
-			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-		else if (s1[i] == '\0')
+		if (*s1 != *s2)
+			return ((unsigned char)*s1) - ((unsigned char)*s2);
+		else if (*s1 == '\0')
 			return (0);
-		i++;
+		s1++;
+		s2++;
+		n--;
 	}
 	return (0);
 }
-
 int main()
 {
 	char *s1 = "Tengo mucho sueño y hambre";
