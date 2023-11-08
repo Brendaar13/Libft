bf2f0c69c81adeba7ccd3433149a623715507ba2
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-08 17:06:58 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,7 +5,7 @@ int	ft_atoi(const char *str)
 
 	result = 0;
 	sign = 1;
-	while (*str == 32 || (*str >= 9 && *str <= 13))
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
 	str++;
 	if (*str == '-')
 	sign *= -1;
