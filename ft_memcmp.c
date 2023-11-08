71084d8584932bfce893ff1d19acefba1ea3d74d
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-08 16:43:48 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,13 +1,11 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t				i;
 	unsigned const char	*str1;
 	unsigned const char	*str2;
 
-	i = 0;
 	str1 = ((unsigned char *)s1);
 	str2 = ((unsigned char *)s2);
-	while (i < n)
+	while (n > 0)
 	{
 		if (*str1 != *str2)
 		{
@@ -15,7 +13,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		}
 		str1++;
 		str2++;
-		i++;
+		n--;
 	}
 	return (0);
 }
