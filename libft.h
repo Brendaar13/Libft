12939188379573430f0c2dbd33dbca5c37deff71
sergy <sergy.lopez.21@gmail.com>
2023-11-09 23:55:56 +0100

    fixed

--- libft.h
+++ libft.h
@@ -1,5 +1,6 @@
 #ifndef LIBFT_H
 #define LIBFT_H
+#include <string.h>
 
 int ft_atoi(const char *str);
 void    *ft_memset(void *b, int c, size_t len);
@@ -10,7 +11,7 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
-char	*ft_itoa(int n):
+char	*ft_itoa(int n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
