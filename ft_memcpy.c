15b5a8a927842401d33e118357123524466004b1
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-14 21:35:26 +0100

    Revisar main

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -18,12 +18,7 @@ int main ()
 {
 	char src[] = "Me quiero mori";
 	char dst[5];
-	//char dst2[5];
 	size_t n = ft_strlen(src) + 1;
-
-	//ft_memcpy(src, dst, 10);
-	//printf("%s\n", src);
 	printf("%s\n", ft_memcpy(dst, src, n));
-	//printf("%s\n", memcpy(dst2, src, n));
 	return (0);
 }
