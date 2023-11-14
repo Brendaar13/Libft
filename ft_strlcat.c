8458b86757cee4b11615af8d832a06d77557a9eb
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-14 21:45:52 +0100

    Listo

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -24,7 +24,7 @@
 // 	return (dstlen + srclen);
 // }
 
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+/*size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
         size_t  dstlen;
         size_t  i;
@@ -39,10 +39,37 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
         }
         dst[dstlen + i] = '\0';
 	return (dstlen + i + 1);
+}*/
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dstlen;
+	size_t	srclen;
+	size_t	i;
+
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	i = 0;
+	if (size == 0)
+		return (srclen);
+	if (size <= dstlen)
+		return (srclen += size);
+	else
+		srclen += dstlen;
+	while (src[i] != '\0' && dstlen < size - 1 && dst != src)
+	{
+		dst[dstlen] = src[i];
+		i++;
+		dstlen++;
+	}
+	dst[dstlen] = '\0';
+	return (srclen);
 }
+
 // Concatena todos los bytes posibles de src a dts. Nunca tiene que machacar el byte nulo ('\0') el final del string, si esto ocurriese significa que el string dst está
 // mal formateado o el size es incorrecto. El length de dst es size - 1, ya que se reserva un byte para el byte nulo del final de string.
-// int main ()
+
+int main ()
 // {
 //         char src[] = "tengo suenioooo";
 //         char dst[13] = "ayudaa";
