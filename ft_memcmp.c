e6793b2dd02bb32a75ffd8b9100be4897300755e
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2023-11-20 11:30:10 +0100

    Listo

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -20,14 +20,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 int main()
 {
-	char *s1 = {0x13};
-	char *s2 = {0x13, 0x77, 0x88, 0x93};
+	char *s1 = "adios";
+	char *s2 = "adia";
 	size_t n = sizeof(s1);
 
 	int result = ft_memcmp(s1, s2, n);
 
-	printf("0x%02X\n", result);
-	return (0)
-		;
+	printf("%d\n", result);
+	return (0);
 }
-Revisar el main
