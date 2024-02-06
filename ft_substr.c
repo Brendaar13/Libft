2dc1a3ed38c04207f0ea23b81e4f96d5171d7341
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2024-02-06 17:02:40 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,25 +1,25 @@
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
-	char	*str;
+	char	*alm;
 	size_t	i;
 	size_t	clen;
 
 	i = 0;
-	clen = ft_strlen(s);
+	clen = ft_strlen(str);
 	if (start >= clen)
 		return (ft_strdup(""));
 	if (len > clen - start)
 		len = clen - start;
-	str = ft_calloc((len + 1), sizeof(char));
-	if (!str)
+	alm = ft_calloc((len + 1), sizeof(char));
+	if (!alm)
 		return (NULL);
 	while (len > i && start < clen)
 	{
-		str[i] = s[i + start];
+		alm[i] = str[i + start];
 		i++;
 	}
-	str[i] = '\0';
-	return (str);
+	alm[i] = '\0';
+	return (alm);
 }
 
 /*int main ()
