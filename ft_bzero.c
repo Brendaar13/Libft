294a9aba3cb6181f1cca5f78a7227e8db4776669
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2024-02-27 20:58:46 +0100

    Listo

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,11 +3,13 @@ void	ft_bzero(void *ptr, size_t n)
 	ft_memset(ptr, 0, n);
 }
 
-int main ()
+/*int main ()
 {
-	char buffer[5] = "hola";
-	printf("Before: %s\n", buffer);
-	ft_bzero(buffer, sizeof(buffer));
-	printf("After: %s\n", buffer);
-	return (0);
-}
+	char str[]= "Holaaa";
+
+	printf("Before: %s\n", str);
+	ft_bzero(str, 1);
+	printf("After: %s\n", str);
+
+	return 0;
+}*/
