6c3d776fffd9ce6a5519ea1314984b36f8617ffa
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2024-02-28 12:19:29 +0100

    Listo

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,6 +1,6 @@
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t c)
 {
-	while (n > 0)
+	while (c > 0)
 	{
 		if (*s1 != *s2)
 			return ((unsigned char)*s1) - ((unsigned char)*s2);
@@ -8,7 +8,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 			return (0);
 		s1++;
 		s2++;
-		n--;
+		c--;
 	}
 	return (0);
 }
